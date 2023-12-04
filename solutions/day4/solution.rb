ba6201f3a25cd53aa8b23a5b5c58534b427ed66d
Jesper Lugner <jesper.lugner@icloud.com>
2023-12-04 06:32:03 +0100

    Add day 4

--- solutions/day4/solution.rb
+++ solutions/day4/solution.rb
@@ -4,14 +4,28 @@ module Solutions
   module Day4
     module_function
 
-    def parse(input); end
+    def parse(input)
+      input.lines.map do |line|
+        match = line.match(/Card *\d+: (?<ours>.*) \| (?<winning>.*)/)
+        (match[:ours].split.map(&:to_i) & match[:winning].split.map(&:to_i)).count
+      end
+    end
 
     def part1(input)
-      raise NotImplementedError
+      input.sum { |wins| wins.positive? ? 1 * (2**(wins - 1)) : 0 }
     end
 
     def part2(input)
-      raise NotImplementedError
+      copies = Array.new(input.size, 1)
+      input.each_with_index do |wins, index|
+        copies[index].times do
+          wins.times do |win_count|
+            copies[index + win_count + 1] += 1 if index + win_count + 1 < copies.size
+          end
+        end
+      end
+
+      copies.sum
     end
   end
 end
--- test/solutions/day4_test.rb
+++ test/solutions/day4_test.rb
@@ -8,18 +8,23 @@ module Solutions
       @solver = Solutions::Day4
     end
 
+    TEST_INPUT = <<~INPUT
+      Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+      Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+      Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+      Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+      Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+      Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    INPUT
+
     def test_parse; end
 
     def test_part1
-      assert_raises NotImplementedError do
-        @solver.part1(nil)
-      end
+      assert_equal(13, @solver.part1(@solver.parse(TEST_INPUT)))
     end
 
     def test_part2
-      assert_raises NotImplementedError do
-        @solver.part2(nil)
-      end
+      assert_equal(30, @solver.part2(@solver.parse(TEST_INPUT)))
     end
   end
 end
