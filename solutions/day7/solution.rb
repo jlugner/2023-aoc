4765f03679e789218eb591115fa50759bd288d03
Jesper Lugner <jesper.lugner@icloud.com>
2023-12-08 07:42:25 +0100

    Add day 7

--- solutions/day7/solution.rb
+++ solutions/day7/solution.rb
@@ -16,15 +16,17 @@ module Solutions
 
       def card_strengths = hand.chars.map { CARD_STRENGTH_ORDER.index(_1) }
 
-      def score = case occurrences_sorted
-                  in [5]          then 7
-                  in [4, _]       then 6
-                  in [3, 2]       then 5
-                  in [3, _, _]    then 4
-                  in [2, 2, _]    then 3
-                  in [2, _, _, _] then 2
-                  else                 1
-                  end
+      def score
+        case occurrences_sorted
+        in [5]          then 7
+        in [4, _]       then 6
+        in [3, 2]       then 5
+        in [3, _, _]    then 4
+        in [2, 2, _]    then 3
+        in [2, _, _, _] then 2
+        else                 1
+        end
+      end
 
       def occurrences_sorted
         occurrences = hand.chars.tally
