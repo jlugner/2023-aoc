8736e05215ced6e06c3c44d62480119b7a9fd3c0
Jesper Lugner <jesper.lugner@icloud.com>
2023-12-09 11:50:28 +0100

    Cleanup day 9

--- solutions/day9/solution.rb
+++ solutions/day9/solution.rb
@@ -4,26 +4,12 @@ module Solutions
   module Day9
     module_function
 
-    def parse(input)
-      input.lines.map { _1.split.map(&:to_i) }
-    end
+    def parse(input) = input.lines.map { _1.split.map(&:to_i) }
+    def part1(input) = input.sum { build_change_history(_1).sum(&:last) }
+    def part2(input) = input.map(&:reverse).sum { build_change_history(_1).sum(&:last) }
 
-    def part1(input)
-      input.sum { extrapolate_next_value(_1) }
-    end
-
-    def part2(input)
-      input.map(&:reverse).sum { extrapolate_next_value(_1) }
-    end
-
-    def build_change_history(value_history)
-      [value_history].tap do |history|
-        history << history.last.each_cons(2).map { _2 - _1 } until history.last.all?(&:zero?)
-      end
-    end
-
-    def extrapolate_next_value(value_history)
-      build_change_history(value_history).sum(&:last)
+    def build_change_history(value_history) = [value_history].tap do |history|
+      history << history.last.each_cons(2).map { _2 - _1 } until history.last.all?(&:zero?)
     end
   end
 end
